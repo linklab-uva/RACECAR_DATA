5227f7449f9e6b303f155c11e7e3ee7806ce2719
ark8su <ark8su@virginia.edu>
2023-02-13 14:27:08 -0500

    updated local odom generation script to accept configs

--- local_odom_conversion.py
+++ local_odom_conversion.py
@@ -11,12 +11,14 @@ from tqdm import tqdm
 from copy import copy, deepcopy
 
 import os
+import sys
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import pygeodesy
 import math
 import yaml
+
 def guess_msgtype(path: Path) -> str:
     """Guess message type name from path."""
     name = path.relative_to(path.parents[2]).with_suffix('')
@@ -28,8 +30,9 @@ def absoluteFilePaths(directory):
     for dirpath,_,filenames in os.walk(directory):
         for f in filenames:
             yield os.path.abspath(os.path.join(dirpath, f))
+
 # Import ROS2 Types
-abs_msg_list = absoluteFilePaths('novatel_oem7_msgs/msg')
+abs_msg_list = absoluteFilePaths('novatel_gps_msgs/msg')
 add_types = {}
 
 for pathstr in abs_msg_list:
@@ -37,7 +40,16 @@ for pathstr in abs_msg_list:
     msgdef = msgpath.read_text(encoding='utf-8')
     add_types.update(get_types_from_msg(msgdef, guess_msgtype(msgpath)))
 
+abs_msg_list = absoluteFilePaths('novatel_oem7_msgs/msg')
+for pathstr in abs_msg_list:
+    msgpath = Path(pathstr)
+    msgdef = msgpath.read_text(encoding='utf-8')
+    add_types.update(get_types_from_msg(msgdef, guess_msgtype(msgpath)))
+
 register_types(add_types)
+from rosbags.typesys.types import novatel_gps_msgs__msg__NovatelPosition as NovatelPosition
+from rosbags.typesys.types import novatel_gps_msgs__msg__NovatelVelocity as NovatelVelocity
+from rosbags.typesys.types import novatel_gps_msgs__msg__NovatelMessageHeader as NovatelMessageHeader
 from rosbags.typesys.types import novatel_oem7_msgs__msg__BESTPOS as BESTPOS
 from rosbags.typesys.types import novatel_oem7_msgs__msg__BESTVEL as BESTVEL
 from rosbags.typesys.types import novatel_oem7_msgs__msg__Oem7Header as Oem7Header
@@ -70,8 +82,8 @@ class OdomConverter(object):
             # messages() accepts connection filters
             connections = [x for x in reader.connections if x.topic in topics]
             for connection, timestamp, rawdata in tqdm(reader.messages(connections=connections)):
-                msg = deserialize_cdr(rawdata, connection.msgtype)
                 if connection.msgtype in topic_types:
+                    msg = deserialize_cdr(rawdata, connection.msgtype)
                     topic_dict[connection.topic].append((timestamp,msg))
 
         return topic_dict
@@ -107,9 +119,9 @@ class OdomConverter(object):
         gpsmap = pygeodesy.LocalCartesian(origin[0], origin[1], origin[2])
 
         prev_pos = [0.0,0.0,0.0]
-        prev_time = gps_messages[0][0]-10000
+        prev_time = gps_messages[0][0]
 
-        for ros_time, bestpos in tqdm(gps_messages):
+        for ros_time, bestpos in tqdm(gps_messages[1:]):
 
             
             bestvel = None
@@ -207,22 +219,128 @@ class OdomConverter(object):
             local_odom_arr.append([ros_time, local_odom, gps_time])
 
         return local_odom_arr
-
-    def nav2odom(gps_messages, track):
+    
+    def gen_local_odom_euro(self, gps_messages, vel_messages):
 
         deg2rad = math.pi/180.0
 
         local_odom_arr = []
-        if track == 'ims':
-            origin = (39.795398310617, -86.23476471193612, 223)
-        elif track == 'lvms':
-            origin = (36.272371177449344, -115.01030828834901, 594)
+        origin = self.cfg["track"]["origin"]
         gpsmap = pygeodesy.LocalCartesian(origin[0], origin[1], origin[2])
 
         prev_pos = [0.0,0.0,0.0]
-        prev_time = gps_messages[0][0]-10000
+        prev_time = gps_messages[0][0]
 
-        for ros_time, msg in tqdm(gps_messages):
+        for ros_time, bestpos in tqdm(gps_messages[1:]):
+
+            
+            bestvel = None
+
+            ros_time_vel = 0
+
+            # Parse BESTPOS
+
+            ros_header = bestpos.header
+            gps_header = bestpos.novatel_msg_header
+            gps_time   = gps_header.gps_seconds * 1000
+            lat        = bestpos.lat
+            lon        = bestpos.lon
+            hgt        = bestpos.height
+            lat_stdev  = bestpos.lat_sigma  
+            lon_stdev  = bestpos.lon_sigma  
+            hgt_stdev  = bestpos.height_sigma
+
+            novel = True
+            # Find Corresponding BESTVEL
+            for time,msg in vel_messages:
+                if msg.novatel_msg_header.gps_seconds * 1000 == gps_time:
+                    ros_time_vel = time
+                    bestvel = msg
+                    novel = False
+                    break
+                if msg.novatel_msg_header.gps_seconds * 1000 > gps_time:
+                    break 
+            
+
+            # GPS 2 LOCAL CARTESIAN
+            local_tuple = gpsmap.forward(lat, lon, hgt)
+            if novel:
+                heading = math.atan2(local_tuple[1] - prev_pos[1], local_tuple[0] - prev_pos[0])
+                hor_speed = (((local_tuple[1] - prev_pos[1])**2+(local_tuple[0] - prev_pos[0])**2)**(0.5))/(ros_time-prev_time)
+                # quat    = R.from_euler('z',heading).as_quat()
+            else:
+                ros_header_v = bestvel.header
+                gps_header_v = bestvel.novatel_msg_header
+                gps_time_v   = gps_header_v.gps_seconds
+                trk_gnd      = bestvel.track_ground
+                latency      = bestvel.latency
+                hor_speed    = bestvel.horizontal_speed
+                ver_speed    = bestvel.vertical_speed
+                heading      =-trk_gnd*deg2rad
+            
+            trk_r  = R.from_euler('z',heading)
+            quat = (trk_r).as_quat()
+            rot_stdev = (math.pi/(2*180))
+
+            prev_pos[0] = local_tuple[0]
+            prev_pos[1] = local_tuple[1]
+            prev_pos[2] = local_tuple[2]
+            
+            prev_time   =  ros_time
+            
+            
+            ros_header.frame_id = 'map'
+            # Populate Odom Message
+            gps_to_ramg : np.ndarray = np.eye(4)
+            gps_to_ramg[0,3]=-1.606
+            maptogps : np.ndarray             = np.eye(4)
+            maptogps[0:3,3]                   = np.asarray([local_tuple[0], local_tuple[1], local_tuple[2]])
+            maptogps[0:3,0:3]                 = R.from_quat(np.asarray([quat[0], quat[1], quat[2], quat[3]])).as_matrix()
+            ramg_pose : np.ndarray            = np.matmul(maptogps, gps_to_ramg)
+            ramg_quat : np.ndarray            = R.from_matrix(ramg_pose[0:3,0:3]).as_quat()
+            
+            
+            odom_point   = Point(x=ramg_pose[0,3],y=ramg_pose[1,3],z=ramg_pose[2,3])
+            odom_quat    = Quaternion(x=ramg_quat[0],y=ramg_quat[1],z=ramg_quat[2],w=ramg_quat[3])
+            linear_vel   = Vector3(x=hor_speed,y=0.0,z=0.0)
+            angular_vel  = Vector3(x=0.0,y=0.0,z=0.0)
+            odom_pose    = Pose(position=odom_point,orientation=odom_quat)
+            odom_twist   = Twist(linear=linear_vel,angular=angular_vel)
+            cov_pose     = np.zeros(36)
+            cov_pose[0]  = lon_stdev*lon_stdev
+            cov_pose[7]  = lat_stdev*lat_stdev
+            cov_pose[14] = hgt_stdev*hgt_stdev
+            cov_pose[21] = rot_stdev*rot_stdev
+            cov_pose[28] = rot_stdev*rot_stdev
+            cov_pose[35] = rot_stdev*rot_stdev
+            cov_twist    = np.zeros(36)
+            cov_twist[0] = .125*.125
+            cov_twist[7] = .0025
+            cov_twist[14]= .0025
+            odom_posec   = PoseWithCovariance(pose=odom_pose,covariance=cov_pose)
+            odom_twistc  = TwistWithCovariance(twist=odom_twist,covariance=cov_twist)
+            
+            local_odom = Odometry(header = ros_header,
+                                child_frame_id = 'rear_axle_middle_ground',
+                                pose = odom_posec,
+                                twist= odom_twistc)
+
+            local_odom_arr.append([ros_time, local_odom, gps_time])
+
+        return local_odom_arr
+
+    def nav2odom(self, gps_messages):
+
+        deg2rad = math.pi/180.0
+
+        local_odom_arr = []
+        origin = self.cfg["track"]["origin"]
+        gpsmap = pygeodesy.LocalCartesian(origin[0], origin[1], origin[2])
+
+        prev_pos = [0.0,0.0,0.0]
+        prev_time = gps_messages[0][0]
+
+        for ros_time, msg in tqdm(gps_messages[1:]):
 
             
             bestvel = None
@@ -293,7 +411,7 @@ class OdomConverter(object):
 
         return local_odom_arr      
 
-    def write_merged_bag(self, ego_bag_file, target_bag, ego_namespace, obj_namespace, ego_odom, obj_odom, start_time, end_time):
+    def write_merged_bag(self, ego_bag_file, target_bag, ego_namespace, obj_namespace, ego_odom, obj_odom, start_time, end_time, lidar_topics):
         ego_sensors=[]
         conn_map = {}
 
@@ -307,10 +425,11 @@ class OdomConverter(object):
 
         with Reader(ego_bag_file) as reader, Writer(target_bag) as writer:
             for connection in reader.connections:
-                if (connection.topic == '/tf'):
+                if (connection.topic in '/tf'):
                     continue
-                ego_sensors.append(connection.topic)
-                conn_map[connection.id] = writer.add_connection('{}'.format(connection.topic), connection.msgtype, 'cdr', '')
+                if ('novatel' in connection.topic) or ('camera' in connection.topic) or ('radar' in connection.topic) or (connection.topic in lidar_topics):
+                    ego_sensors.append(connection.topic)
+                    conn_map[connection.id] = writer.add_connection('/{}{}'.format(ego_namespace,connection.topic), connection.msgtype, 'cdr', '')
 
             connections = [x for x in reader.connections if x.topic in ego_sensors]
             for connection, timestamp, rawdata in tqdm(reader.messages(connections=connections)):
@@ -335,7 +454,7 @@ class OdomConverter(object):
                 message = odom[1]
                 writer.write(obj_connection, timestamp, serialize_cdr(message,msgtype))
 
-    def write_solo_bag(self, ego_bag_file, target_bag, ego_namespace, ego_odom, start_time, end_time):
+    def write_solo_bag(self, ego_bag_file, target_bag, ego_namespace, ego_odom, start_time, end_time, lidar_topics):
         ego_sensors=[]
         conn_map = {}
 
@@ -343,8 +462,9 @@ class OdomConverter(object):
             for connection in reader.connections:
                 if (connection.topic == '/tf'):
                     continue
-                ego_sensors.append(connection.topic)
-                conn_map[connection.id] = writer.add_connection('{}'.format(connection.topic), connection.msgtype, 'cdr', '')
+                if ('novatel' in connection.topic) or ('camera' in connection.topic) or ('radar' in connection.topic) or (connection.topic in lidar_topics):
+                    ego_sensors.append(connection.topic)
+                    conn_map[connection.id] = writer.add_connection('/{}{}'.format(ego_namespace,connection.topic), connection.msgtype, 'cdr', '')
 
             connections = [x for x in reader.connections if x.topic in ego_sensors]
             for connection, timestamp, rawdata in tqdm(reader.messages(connections=connections)):
@@ -363,60 +483,86 @@ class OdomConverter(object):
                 writer.write(ego_connection, timestamp, serialize_cdr(message,msgtype))
 
     def process_data(self):
+        multi = self.cfg["multi"]
         track = self.cfg["track"]["name"]
         start_time = self.cfg["track"]["start_time"]
         end_time = self.cfg["track"]["end_time"]
         ego_team = self.cfg["ego"]["team"]
         ego_num = self.cfg["ego"]["number"]
         ego_source = self.cfg["ego"]["source_file"]
+        ego_target = self.cfg["ego"]["target_file"]
         ego_path = os.path.join('../../data/RAW_ROSBAG', ego_team, track, ego_source)
-        ego_target_path = os.path.join('../../data/LOCAL_ODOM', ego_team, track, ego_source)
+        ego_target_path = os.path.join('../../data/LOCAL_ODOM', ego_team, track, ego_target)
         ego_topics = self.cfg["ego"]["gps_topics"]
         ego_types = self.cfg["ego"]["topic_types"]
+        ego_lidar = self.cfg["ego"]["lidar_topics"]
+        ego_write = self.cfg["ego"]["write_to_bag"]
 
-        target_team = self.cfg["target"]["team"]
-        ego_num = self.cfg["target"]["number"]
-        target_source = self.cfg["target"]["source_file"]
-        target_path = os.path.join('../../data/RAW_ROSBAG', target_team, track, target_source)
-        target__target_path = os.path.join('../../data/LOCAL_ODOM', target_team, track, target_source)
-        target_topics = self.cfg["target"]["gps_topics"]
-        target_types = self.cfg["target"]["topic_types"]
 
         ego_data = self.read_bag_file(ego_path, ego_topics, ego_types)
-        target_data = self.read_bag_file(target_path, target_topics, target_types)
 
         if self.cfg["ego"]["novatel"]:
             ego_odom = self.gen_local_odom(ego_data[ego_topics[0]], ego_data[ego_topics[1]])
         else:
-            ego_odom = self.nav2odom(ego_data[ego_topics[0]])
+            ego_odom = self.gen_local_odom_euro(ego_data[ego_topics[0]], ego_data[ego_topics[1]])
 
-        if self.cfg["target"]["novatel"]:
-            target_odom = self.gen_local_odom(target_data[target_topics[0]], target_data[target_topics[1]])
-        else:
-            target_odom = self.nav2odom(target_data[target_topics[0]])
-
+        
         pruned_ego_odom = [ego_odom[i] for i, y in enumerate(ego_odom) if (y[0]*1e-9 > start_time) and (y[0]*1e-9 < end_time)]
-        pruned_target_odom = [target_odom[i] for i, y in enumerate(target_odom) if (y[0]*1e-9 > start_time) and (y[0]*1e-9 < end_time)]
 
-        write_merged_bag(ego_bag_file=ego_source, 
-                 target_bag = ego_target_path,
-                 ego_namespace='vehicle_{}'.format(ego_num),
-                 obj_namespace='vehicle_{}'.format(target_num),
-                 ego_odom=pruned_ego_odom,
-                 obj_odom=pruned_target_odom,
-                 start_time=start_time,
-                 end_time=end_time)
+        if multi:
+            target_team = self.cfg["target"]["team"]
+            target_num = self.cfg["target"]["number"]
+            target_source = self.cfg["target"]["source_file"]
+            target_target = self.cfg["target"]["target_file"]
+            target_path = os.path.join('../../data/RAW_ROSBAG', target_team, track, target_source)
+            target_target_path = os.path.join('../../data/LOCAL_ODOM', target_team, track, target_target)
+            target_topics = self.cfg["target"]["gps_topics"]
+            target_types = self.cfg["target"]["topic_types"]
+            target_lidar = self.cfg["target"]["lidar_topics"]
+            target_write = self.cfg["target"]["write_to_bag"]
 
-        write_merged_bag(ego_bag_file=target_source, 
-                 target_bag = target_target_path,
-                 ego_namespace='vehicle_{}'.format(target_num),
-                 obj_namespace='vehicle_{}'.format(ego_num),
-                 ego_odom=pruned_target_odom,
-                 obj_odom=pruned_ego_odom,
-                 start_time=start_time,
-                 end_time=end_time)
+            target_data = self.read_bag_file(target_path, target_topics, target_types)
+
+
+            if self.cfg["target"]["novatel"]:
+                target_odom = self.gen_local_odom(target_data[target_topics[0]], target_data[target_topics[1]])
+            else:
+                target_odom = self.gen_local_odom_euro(target_data[target_topics[0]], target_data[target_topics[1]])
+
+            pruned_target_odom = [target_odom[i] for i, y in enumerate(target_odom) if (y[0]*1e-9 > start_time) and (y[0]*1e-9 < end_time)]
+
+            if ego_write:
+                self.write_merged_bag(ego_bag_file=ego_path, 
+                        target_bag = ego_target_path,
+                        ego_namespace='vehicle_{}'.format(ego_num),
+                        obj_namespace='vehicle_{}'.format(target_num),
+                        ego_odom=pruned_ego_odom,
+                        obj_odom=target_odom,
+                        start_time=start_time,
+                        end_time=end_time,
+                        lidar_topics=ego_lidar)
+                
+            if target_write:
+                self.write_merged_bag(ego_bag_file=target_path, 
+                        target_bag = target_target_path,
+                        ego_namespace='vehicle_{}'.format(target_num),
+                        obj_namespace='vehicle_{}'.format(ego_num),
+                        ego_odom=pruned_target_odom,
+                        obj_odom=pruned_ego_odom,
+                        start_time=start_time,
+                        end_time=end_time,
+                        lidar_topics=target_lidar)
+        else:
+            self.write_solo_bag(ego_bag_file=ego_path, 
+                        target_bag = ego_target_path,
+                        ego_namespace='vehicle_{}'.format(ego_num),
+                        ego_odom=pruned_ego_odom,
+                        start_time=start_time,
+                        end_time=end_time,
+                        lidar_topics=ego_lidar)
 
 
 if __name__ == '__main__':
-    converter = OdomConverter(cfg = 'conversion_configs/cav_slow_poli.yaml')
+    config_file = str(sys.argv[1])
+    converter = OdomConverter(cfg = config_file)
     converter.process_data()
---
+++ novatel_gps_msgs/CHANGELOG.rst
@@ -0,0 +1,70 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package novatel_gps_msgs
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+3.9.0 (2019-09-04)
+------------------
+* Use GPGGA quality to set GPSFix status (`#55 <https://github.com/swri-robotics/novatel_gps_driver/issues/55>`_)
+* Add GPHDT message (`#51 <https://github.com/swri-robotics/novatel_gps_driver/issues/51>`_)
+* Add DUALANTENNAHEADING msg (`#46 <https://github.com/swri-robotics/novatel_gps_driver/issues/46>`_)
+* Add HEADER2 msg (`#43 <https://github.com/swri-robotics/novatel_gps_driver/issues/43>`_)
+* Add BESTXYZ msg (`#42 <https://github.com/swri-robotics/novatel_gps_driver/issues/42>`_)
+* Contributors: Marcel Zeilinger, Michael McConnell, P. J. Reed
+
+3.8.0 (2019-06-04)
+------------------
+* Add clocksteering parsing (`#40 <https://github.com/swri-robotics/novatel_gps_driver/issues/40>`_)
+* Contributors: Matthew
+
+3.7.0 (2019-03-22)
+------------------
+
+3.6.0 (2018-10-09)
+------------------
+* Add NovatelUtmPosition message
+* Add Inspvax message (#27)
+* Contributors: Ellon Paiva Mendes, Sagnik Basu
+
+3.5.0 (2018-07-17)
+------------------
+* Update documentation
+* Contributors: P. J. Reed
+
+3.4.0 (2017-10-06)
+------------------
+* Add service to novatel_gps_nodelet that issues a FRESET command on the indicated target, or STANDARD if none is provided
+* Contributors: Matthew Bries, P. J. Reed
+
+3.3.0 (2017-08-31)
+------------------
+* Publish sensor_msgs/Imu messages
+* Code cleanup
+* Contributors: Edward Venator, P. J. Reed
+
+3.2.0 (2017-07-21)
+------------------
+* Publish IMU data from NovAtel SPAN devices
+* Contributors: P. J. Reed
+
+3.1.0 (2017-06-27)
+------------------
+
+3.0.1 (2017-05-08)
+------------------
+
+3.0.0 (2017-04-03)
+------------------
+* Rename novatel_msgs to novatel_gps_msgs
+* Contributors: P. J. Reed
+
+2.9.0 (2017-01-11)
+------------------
+
+2.8.0 (2017-01-05)
+------------------
+
+2.7.2 (2016-11-28)
+------------------
+
+2.7.0 (2015-09-24 15:37:00 -0500)
+---------------------------------
